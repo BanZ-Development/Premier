697261c0651acf488ee2678df9c00ced9da80b97
Lucky <swordslayer07@gmail.com>
2024-01-13 01:49:51 -0700

    small chnages

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ anime({
     delay: 200
 })
 
-function slide() {
-    let slides = document.getElementById('somethin')
+function slide(change) {
+    let slides = document.querySelectorAll('.testimonial')
+    for (i=0;i < slides.length-1;i++) {
+        console.log(slides[i])
+        if (window.getComputedStyle(slides[i], null).display == 'block') {
+            let currentSlide = i;
+            slides[i].style.display = 'none';
+            slides[i+change].style.display = 'block';
+            console.log(slides[i+1])
+        }
+    }
 }
