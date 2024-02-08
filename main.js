b0e54f874e7de65d77ce586356888179694ffb81
Lucky <swordslayer07@gmail.com>
2024-02-07 19:57:50 -0700

    footer and other stuff

--- main.js
+++ main.js
@@ -4,17 +4,26 @@ anime({
     easing: 'linear',
     duration: 800,
     delay: 200
-})
+});
+
+anime({
+    targets: '.main-container',
+    translateX: [-600, 0],
+    duration: 500,
+    delay: 200
+});
 
 function slide(change) {
     let slides = document.querySelectorAll('.testimonial')
-    for (i=0;i < slides.length-1;i++) {
-        console.log(slides[i])
-        if (window.getComputedStyle(slides[i], null).display == 'block') {
-            let currentSlide = i;
-            slides[i].style.display = 'none';
-            slides[i+change].style.display = 'block';
-            console.log(slides[i+1])
+    if (change == -1) {
+
+    } else {
+        for (i=0;i < slides.length-1;i++) {
+            if (window.getComputedStyle(slides[i], null).display == 'block') {
+                slides[i].style.display = 'none';
+                slides[i+change].style.display = 'block';
+                console.log(slides[i-1]);
+            }
         }
     }
 }
