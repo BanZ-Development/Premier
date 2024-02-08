30370493b78f0d26bff3943e1359fc584b5f8aa6
Lucky <68414136+Raidlucky@users.noreply.github.com>
2024-02-08 02:15:38 -0700

    Landing Page & Insurance
    Finished Landing Page for now, working on insurance page

--- main.js
+++ main.js
@@ -15,14 +15,32 @@ anime({
 
 function slide(change) {
     let slides = document.querySelectorAll('.testimonial')
-    if (change == -1) {
-
-    } else {
-        for (i=0;i < slides.length-1;i++) {
+    if (change == +1) {
+        for (i=0; i < slides.length;i++) {
             if (window.getComputedStyle(slides[i], null).display == 'block') {
-                slides[i].style.display = 'none';
-                slides[i+change].style.display = 'block';
-                console.log(slides[i-1]);
+                let currentSlide = i;
+                if (currentSlide == slides.length-1) {
+                    slides[i].style.display = 'none';
+                    slides[0].style.display = 'block';
+                } else {
+                    slides[i].style.display = 'none';
+                    slides[i+1].style.display = 'block';
+                    break;
+                }
+            }
+        }
+    } else {
+        for (i=0; i < slides.length;i++) {
+            if (window.getComputedStyle(slides[i], null).display == 'block') {
+                currentSlide = i;
+                if (currentSlide == 0) {
+                    slides[i].style.display = 'none';
+                    slides[slides.length-1].style.display = 'block';
+                    break;
+                } else {
+                    slides[i].style.display = 'none';
+                    slides[i-1].style.display = 'block';
+                }
             }
         }
     }
